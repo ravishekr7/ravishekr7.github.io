3551227dacaab8c924affa27577a5db626d9c9c5
ravishekr7 <ravishekr7@gmail.com>
2024-05-12 15:17:18 +0530

    some styles to the existing page

---
+++ app/components/layout.jsx
@@ -0,0 +1,15 @@
+
+import Navbar from './navbar';
+import Footer from './footer';
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <main></main>
+      <Footer />
+    </>
+  )
+}
+
+export default Layout;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
+import Footer from "./components/footer";
+import Layout from "./components/layout";
 export default function Home() {
-    return <h1>Ravishek Kumar Ranjan</h1>;
+    return <Layout></Layout>;
 }
